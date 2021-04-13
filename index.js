e2b8b44fbeb12c7aa14dcf0758b58b79914013e2
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-04-13 22:35:49 +0530

    with initial api working changes

--- index.js
+++ index.js
@@ -13,18 +13,51 @@ function createDomMani(ele,elementClass="",...arr){
     }
     return element1;
 }
+/*<script src="https://apis.google.com/js/api.js"></script>
+    <h1>WeTunnel </h1>
+    <h2>Ease your YouTube journey</h2>
 
-let url_youtubeapi = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true' 
+    <a href="privacypolicy.html">Privacy policy</a>
+    <a href="termsofservice.html">Terms of service</a>
+     */
 
-
-fetch(url_youtubeapi,{
-    headers:{
-        "Authorization": "Bearer oauth2-token"
-    }
-})
-.then(response=>response.json())
-.then(data=>{
-  console.log(data)
+function authorize(){
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
   
-})
-.catch(err=>console.log(err));
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
+  
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {'client_id': '590286321652-8q6ovcufajo6n018aogjgaeq8r5ji9be.apps.googleusercontent.com',
+                  'redirect_uri': 'https://wetunnelutube.netlify.app',
+                  'response_type': 'token',
+                  'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
+                  'include_granted_scopes': 'true',
+                  'state': 'pass-through value'};
+  
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'hidden');
+      input.setAttribute('name', p);
+      input.setAttribute('value', params[p]);
+      form.appendChild(input);
+    }
+  
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+
+}
+
+function pinger(){
+  fetch('https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&key=AIzaSyBZBpp5F7A7C9q38arfOfGr209ZTfWIhUc')
+  .then(response=>console.log(response.json()))
+}
+
+
+
+
