9d60c56bfd1ad4c280178fe7f6065f07b79b7b7a
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-04-14 17:10:43 +0530

    running changes 2

--- index.js
+++ index.js
@@ -91,7 +91,27 @@ const myRequest1 = new Request(`https://www.googleapis.com/youtube/v3/playlists?
  
  fetch(myRequest1)
  .then(response=>{return response.json()})
- .then(data=>console.log(data))
+ .then(data=>{
+  
+  console.log(data)
+  console.log(data.kind)
+  })
+
+  const myHeaders2 = new Headers();
+  const myRequest2 = new Request(`https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?part=snippet&q=GoogleDevelopers&type=playlist`, {
+  method: 'GET',
+  headers: myHeaders2,
+  mode: 'cors',
+  cache: 'default',                     
+});
+ 
+ fetch(myRequest2)
+ .then(response=>{return response.json()})
+ .then(data=>{
+  
+  console.log(data)
+  console.log(data.kind)
+  })
  
 }
 
