8796886f7b53463893fe2f5119dbce08e55d23db
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-04-19 23:30:26 +0530

    new UI based only on Bootstrap css

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
 const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
      
 
 function authorize(){
@@ -245,12 +251,7 @@ const DISCOVERY_DOCS = [
 ];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
-const authorizeButton = document.getElementById('authorize-button');
-const signoutButton = document.getElementById('signout-button');
-const content = document.getElementById('content');
-const channelForm = document.getElementById('channel-form');
-const channelInput = document.getElementById('channel-input');
-const videoContainer = document.getElementById('video-container');
+
 
 const defaultChannel = 'techguyweb';
 
