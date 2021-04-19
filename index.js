73912d41f91793b5de7c0c08a5cff667204772cd
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-04-19 14:09:40 +0530

    reverting back the code

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+const authorizeButton = document.getElementById('authorize-button');
      
 
 function authorize(){
