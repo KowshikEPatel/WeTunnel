59ad913cd80b2bba0b6976b83fd907b16e3f7c4f
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-04-19 14:04:47 +0530

    reverting back the code

--- index.js
+++ index.js
@@ -221,6 +221,21 @@ function initClient() {
     });
 }
 
+// Update UI sign in state changes
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    content.style.display = 'block';
+    videoContainer.style.display = 'block';
+    getChannel(defaultChannel);
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+    content.style.display = 'none';
+    videoContainer.style.display = 'none';
+  }
+}
 // Options
 /*
 const CLIENT_ID = '590286321652-8q6ovcufajo6n018aogjgaeq8r5ji9be.apps.googleusercontent.com';
